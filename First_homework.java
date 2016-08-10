452575cd047183a62f5734a3c1a7da01696d9972
meaadh <meaadhassan09@gmail.com>
2016-08-10 15:16:53 -0400

    Add files via upload
    My nametag

---
+++ First_homework.java
@@ -0,0 +1,37 @@
+package hassan;
+
+public class First_homework {
+
+	public static void main(String[] args) 
+	{
+		for(int x= 0;x<=40;x++)
+		{
+			System.out.print("*");
+			
+		}
+		System.out.println("");
+		for(int x= 0;x<=40;x++)
+		{
+			System.out.print("*");
+			
+		}
+		System.out.println("");
+		System.out.println("\n*************Meaad Hassan****************");
+		System.out.println("***********Athen High School*************");
+		System.out.println("**************11th grade*****************");
+		System.out.println("");
+		for(int x= 0;x<=40;x++)
+		{
+			System.out.print("*");
+			
+		}
+		System.out.println("");
+		for(int x= 0;x<=40;x++)
+		{
+			System.out.print("*");
+			
+		}
+
+	}
+
+}
